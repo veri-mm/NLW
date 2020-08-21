d99f79a1bdcabc91c6fb10518182cbfdfee98542
Veridiana Máximo <69319528+veri-mm@users.noreply.github.com>
2020-08-21 18:16:05 -0300

    update

--- public/scripts/addField.js
+++ public/scripts/addField.js
@@ -1,7 +1,7 @@
 // Procurar o botão
 document.querySelector("#add-time")
 //Quando clicar no botão
-.addEventListener('click', cloneField)
+.addEventListener("click", cloneField)
 
 
 //Executar uma ação
